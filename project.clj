6985cb8c69c7a7fc5289fd58304894de6b355a3f
James Reeves <jreeves@weavejester.com>
2017-03-02 05:13:43 +0000

    Make ClojureScript a provided dependency

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.8.51"]
                  [integrant "0.2.1"]]
   :profiles
-  {:dev {:source-paths ["dev/src"]}})
+  {:provided {:dependencies [[org.clojure/clojurescript "1.8.51"]]}
+   :dev      {:source-paths ["dev/src"]}})
