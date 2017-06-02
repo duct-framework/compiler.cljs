0ebc716cec5e40dcdd85d6d348f3a07ecfc121ab
James Reeves <jreeves@weavejester.com>
2017-06-02 19:50:50 +0100

    Release 0.1.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject duct/compiler.cljs "0.1.1"
+(defproject duct/compiler.cljs "0.1.2"
   :description "Integrant methods for compiling ClojureScript"
   :url "https://github.com/duct-framework/compiler.cljs"
   :license {:name "Eclipse Public License"
